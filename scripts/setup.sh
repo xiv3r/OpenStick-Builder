8d3b610d683c3c65c04adbebbe42165dbaced619
Mio <mio@meow.business>
2025-09-08 21:19:04 +0200

    Fix gt build on ARM64 (i hope)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,9 +17,12 @@ rm -f "/etc/locale.gen"
 apt update -qqy
 apt upgrade -qqy --with-new-pkgs
 apt install -qqy --no-install-recommends \
+    build-essential \
     dnsmasq \
     libconfig11 \
     libconfig-dev \
+    libc6-dev \
+    linux-libc-dev \
     locales \
     modemmanager \
     netcat-traditional \
